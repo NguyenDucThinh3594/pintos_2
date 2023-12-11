878632b7de3c7c3522709a298744ac08ac0dee8f
NguyenDucThinh3594 <thinh.nd203594@gmail.com>
2023-12-11 21:05:54 +0700

    complete

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,178 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "process.h"
+#include "pagedir.h"
+#include <threads/vaddr.h>
+#include <filesys/filesys.h>
+#include <devices/shutdown.h>
+#include <filesys/file.h>
+#include <devices/input.h>
+#include <threads/malloc.h>
+#include <threads/palloc.h>
+# define max_syscall 20
+# define USER_VADDR_BOUND (void*) 0x08048000
+
+static void (*syscalls[max_syscall])(struct intr_frame *);
+static void * check_ptr2(const void *vaddr);
+static void exit_special (void);
+struct thread_file * find_file_id(int fd);
+
+void system_call_halt(struct intr_frame* f); /* syscall halt. */
+void system_call_exit(struct intr_frame* f); /* syscall exit. */
+void system_call_execute(struct intr_frame* f); /* syscall exec. */
+void system_call_wait(struct intr_frame* f); /*syscall wait */
+void system_call_write(struct intr_frame* f); /* syscall write */
+
 
 static void syscall_handler (struct intr_frame *);
+/* New method to check the address and pages to pass test sc-bad-boundary2, execute */
+/* Handle the special situation for thread */
+
+void 
+exit_special (void)
+{
+  thread_current()->st_exit = -1;
+  thread_exit ();
+}
+/*check*/
+static int 
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:" : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+void * 
+check_ptr2(const void *vaddr)
+{ 
+  /* Check address */
+  if (!is_user_vaddr(vaddr))
+  {
+    exit_special ();
+  }
+  /* Check the page */
+  void *ptr = pagedir_get_page (thread_current()->pagedir, vaddr);
+  if (!ptr)
+  {
+    exit_special ();
+  }
+
+  uint8_t *check_byteptr = (uint8_t *) vaddr;
+  for (uint8_t i = 0; i < 4; i++) 
+  {
+    if (get_user(check_byteptr + i) == -1)
+    {
+      exit_special ();
+    }
+  }
+
+  return ptr;
+}
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  syscalls[SYS_EXEC] = &system_call_execute;
+  syscalls[SYS_HALT] = &system_call_halt;
+  syscalls[SYS_EXIT] = &system_call_exit; 
+  syscalls[SYS_WAIT] = &system_call_wait;
+  syscalls[SYS_WRITE] = &system_call_write;
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
+  int * p = f->esp;
+  check_ptr2 (p + 1);
+  int type = * (int *)f->esp;
+  if(type <= 0 || type >= max_syscall){
+    exit_special ();
+  }
+  syscalls[type](f);
+}
+
+void 
+system_call_halt (struct intr_frame* f)
+{
+  shutdown_power_off();
+}
+
+void 
+system_call_exit (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  *user_ptr++;
+  /* record the exit status of the process */
+  thread_current()->st_exit = *user_ptr;
   thread_exit ();
+}
+
+void 
+system_call_execute (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  check_ptr2 (*(user_ptr + 1));
+  *user_ptr++;
+  f->eax = process_execute((char*)* user_ptr);
+}
+
+void 
+system_call_wait (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 1);
+  *user_ptr++;
+  f->eax = process_wait(*user_ptr);
+}
+
+void 
+system_call_write (struct intr_frame* f)
+{
+  uint32_t *user_ptr = f->esp;
+  check_ptr2 (user_ptr + 7);
+  check_ptr2 (*(user_ptr + 6));
+  *user_ptr++;
+  int fd = *user_ptr;
+  const char * buffer = (const char *)*(user_ptr+1);
+  off_t size = *(user_ptr+2);
+  if (fd == 1) {//writes to the console
+    putbuf(buffer,size);
+    f->eax = size;
+  }
+  else
+  {
+    /* Write to Files */
+    struct thread_file * thread_file_temp = find_file_id (*user_ptr);
+    if (thread_file_temp)
+    {
+      acquire_lock_f ();
+      f->eax = file_write (thread_file_temp->file, buffer, size);
+      release_lock_f ();
+    } 
+    else
+    {
+      f->eax = 0;//can't write,return 0
+    }
+  }
+}
+
+struct thread_file * 
+find_file_id (int file_id)
+{
+  struct list_elem *e;
+  struct thread_file * thread_file_temp = NULL;
+  struct list *files = &thread_current ()->files;
+  for (e = list_begin (files); e != list_end (files); e = list_next (e)){
+    thread_file_temp = list_entry (e, struct thread_file, file_elem);
+    if (file_id == thread_file_temp->fd)
+      return thread_file_temp;
+  }
+  return false;
 }
